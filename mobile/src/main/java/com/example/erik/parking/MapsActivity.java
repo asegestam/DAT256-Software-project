4f2356cff68ac836fe8a2b1a74a4e4d8168701ac
vdde <36074206+vdde@users.noreply.github.com>
2018-09-14 12:46:05 +0200

    :)

--- mobile/src/main/java/com/example/erik/parking/MapsActivity.java
+++ mobile/src/main/java/com/example/erik/parking/MapsActivity.java
@@ -14,6 +14,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    //hejhejehj
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
