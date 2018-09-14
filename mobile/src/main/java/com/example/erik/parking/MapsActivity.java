b6cf5da84f972734b4075eb71f0a8aee170b98fd
vdde <36074206+vdde@users.noreply.github.com>
2018-09-14 12:30:11 +0200

    allt från början

--- mobile/src/main/java/com/example/erik/parking/MapsActivity.java
+++ mobile/src/main/java/com/example/erik/parking/MapsActivity.java
@@ -10,8 +10,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
