a5b38caba2cdb7c8b521a6d1042990f323b85e60
akes <ake.svensson97@gmail.com>
2018-09-14 12:52:13 +0200

    blank

--- mobile/src/main/java/com/example/erik/parking/MapsActivity.java
+++ mobile/src/main/java/com/example/erik/parking/MapsActivity.java
@@ -14,12 +14,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-<<<<<<< HEAD
-//hejhejake
-=======
 
-
->>>>>>> master
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
