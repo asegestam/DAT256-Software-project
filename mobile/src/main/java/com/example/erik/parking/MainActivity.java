63f2ba45d00123ac4e3f7978005bd373b130f8b6
ThHaugen <36074326+ThHaugen@users.noreply.github.com>
2018-10-09 14:55:43 +0200

    Merge branch 'Theo' of https://github.com/asegestam/DAT256-Software-project into Theo
    # Conflicts:
    #	mobile/src/main/java/com/example/erik/parking/MapsActivity.java

--- mobile/src/main/java/com/example/erik/parking/MainActivity.java
+++ mobile/src/main/java/com/example/erik/parking/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.erik.parking;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -16,6 +19,7 @@ import com.google.android.gms.common.GoogleApiAvailability;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private DrawerLayout mDrawerLayout;
     private static final int ERROR_DIALOG_REQUEST = 9001;
 
 
@@ -25,6 +29,25 @@ public class MainActivity extends AppCompatActivity {
 
         //set contentview to activity_main
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
 
         //Check if google services is ok before init()
         if (isServicesOK()) {
